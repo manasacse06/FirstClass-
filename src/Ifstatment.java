ad4d2c6e972166cd288a409371a61f39ccf779fd
manasa <manasa@manasa-PC>
2022-08-26 23:34:27 -0500

    if statement

---
+++ src/Ifstatment.java
@@ -0,0 +1,18 @@
+
+ class Ifstatment {
+
+	public static void main(String[] args) {
+		
+		int number=10;
+		
+		//check if number is less than 0
+		if(number<0);{
+			System.out.println("The Number Is Negative.");
+		}
+		System.out.println("The Statment Is Blocked");
+		}
+		
+
+	}
+
+
