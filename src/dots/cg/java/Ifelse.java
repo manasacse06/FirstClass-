2ea3f3e951173cf7de17cc2ea998188909a1a3c1
manasa <manasa@manasa-PC>
2022-08-27 00:01:18 -0500

    ifelse

---
+++ src/dots/cg/java/Ifelse.java
@@ -0,0 +1,20 @@
+package dots.cg.java;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		//create string variable
+		
+		String language ="java";
+		
+		//if Statement
+		
+		if(language=="java") {
+		
+		System.out.println("Best programming language");
+		
+
+	}
+
+}
+}
