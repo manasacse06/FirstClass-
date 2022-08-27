a9069922c2d6ec0b5a78b287caf4ec97b0cd9c2e
manasa <manasa@manasa-PC>
2022-08-26 23:53:04 -0500

    if statment

---
+++ src/dots/cg/java/Ifstatment.java
@@ -0,0 +1,22 @@
+package dots.cg.java;
+
+public class Ifstatment {
+
+	public static void main(String[] args) {
+
+		 
+				
+				int number=10;
+				
+				//check if number is less than 0
+				if(number<0);{
+					System.out.println("The Number Is Negative.");
+				}
+				System.out.println("The Statment Is Blocked");
+				}
+				
+
+			
+	}
+
+
