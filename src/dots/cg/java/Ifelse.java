ba3b41659a42c87c34940e833d1c5b3f6fea6693
manasa <manasa@manasa-PC>
2022-08-30 22:00:30 -0500

    Switch

---
+++ src/dots/cg/java/Ifelse.java
@@ -0,0 +1,23 @@
+package dots.cg.java;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		
+		int number=2;
+		if (number > 0){
+		System.out.println("the number is postive");
+		}
+		else if (number < 0){
+		System.out.println(" the number is negtavie ");
+		}
+		else {
+			System.out.println("the number is 0");
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
---
+++ src/dots/cg/java/Switch.java
@@ -0,0 +1,31 @@
+package dots.cg.java;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int x= 20;
+		switch (x) {
+		
+		case 45:
+			System.out.println("the number is 45");
+			
+		case 34:
+			System.out.println("the number is 34");
+			
+		case 3:
+			System.out.println("the number is 3");
+			
+		case 20:
+			System.out.println("the number is 20");
+			System.out.println("found the num");
+			
+			default:
+				  System.out.println("the number did not match any");
+			
+		
+		}
+		System.out.println("i am free from switch");
+
+	}
+
+}
