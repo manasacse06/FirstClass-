aa20550805dc04350b588f58374d2451809596da
manasa <manasa@manasa-PC>
2022-08-30 22:51:26 -0500

    ForLoop

---
+++ src/dots/cg/java/ForLoop.java
@@ -0,0 +1,14 @@
+package dots.cg.java;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int x;
+		
+		for (x= 0; x < 10;  x++){
+			System.out.println("the value of x is :"+ x);
+		}
+		
+	}
+
+}
