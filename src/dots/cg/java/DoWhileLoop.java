301590a6631355c3ef0fcfafa5e9dd1ef1dd8413
manasa <manasa@manasa-PC>
2022-08-31 11:00:56 -0500

    DoWhileLoop

---
+++ src/dots/cg/java/DoWhileLoop.java
@@ -0,0 +1,22 @@
+package dots.cg.java;
+
+
+import java.util.Scanner;
+
+public class DoWhileLoop {  
+	
+	        public static void main(String[] args) {
+
+	   int i = 1, n = 5;{
+
+	    // do...while loop from 1 to 5
+	    do {
+	      System.out.println(i);
+	      i++;
+	    } while(i <= n);
+	    
+	  
+}
+	   
+}
+}
--- src/dots/cg/java/SumWhile.java
+++ src/dots/cg/java/SumWhile.java
@@ -9,26 +9,20 @@ public class SumWhile {
 	    int sum = 0;
 	    int number = 50;
 
-	    // create an object of Scanner class
-	    //Scanner input = new Scanner(System.in);
-
-	    // take integer input from the user
+	 
 	    System.out.println("Enter a number");
-	   // int number = input.nextInt();
-		   
-	    // while loop continues 
-	    // until entered number is positive
+	  
 	    while (number >= 0) {
-	      // add only positive numbers
-	      sum += number;
+	      
+	    sum += number;
 
 	      System.out.println("Enter a number");
-	      //number = input.nextInt();
+	     
 	      number -=5;//
 	    }
 		   
 	    System.out.println("Sum = " + sum);
-	    //input.close();
+	    
 	  }
 	
 
