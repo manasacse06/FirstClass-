5fb6a27ad529923ad74bcdaff17fa753d7573091
manasa <manasa@manasa-PC>
2022-08-31 10:10:11 -0500

    Whileloop

---
+++ src/dots/cg/java/Whileloop.java
@@ -0,0 +1,18 @@
+package dots.cg.java;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		
+		int i=1 ,n = 5;
+		
+	  //While loop from 1 to 5
+		
+		while(i<=n){
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
