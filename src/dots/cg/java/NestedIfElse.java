d8c624722599038ebc7071914b7737a36e7a0c46
manasa <manasa@manasa-PC>
2022-08-31 10:23:40 -0500

    NestedIfElse

---
+++ src/dots/cg/java/NestedIfElse.java
@@ -0,0 +1,31 @@
+package dots.cg.java;
+
+public class NestedIfElse {
+
+	public static void main(String[] args) {
+		  
+		
+		int age =5;
+		String city ="pune";
+		 
+		 if (city == "Pune") {
+			 System.out.println("the person is in pune");
+			 
+			 if(age>18) {
+				 
+				 System.out.println("he/she is eligible to vote");
+			 }
+			 else {
+				 System.out.println("he/she is in pune but not eligible to vote");
+			 }
+		 }
+			 else {
+				 
+				 System.out.println("the person is  not eligible to vote in pune");
+			 }
+					 
+			 }
+			 
+}
+
+
